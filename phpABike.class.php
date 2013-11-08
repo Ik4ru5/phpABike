e139164cad4d2856782dc434504e94ef7a729b62
Ik4ru5 <webmaster@ik4ru5.de>
2013-11-08 11:51:29 +0100

    added some methods

--- phpABike.class.php
+++ phpABike.class.php
@@ -2,36 +2,36 @@
 class phpABike {
 	var $client;
 	var $result = "";
-	var $commonParams = array();
+	var $commonParams = array("UserData" => array("User" => "t_cab_android", "Password" => "3b3cc28469"), "languageUID" => 1, "RequestTime" => 0, "Version" => 1);
 	var $geoPos = array("Latitude" => 0.0, "Longitude" => 0.0);
 	var $cusotmerData = array("Phone" => "", "Password" => "");
-	
+
 	function __construct($url) {
 		$this->client = new SoapClient("https://xml.dbcarsharing-buchung.de/hal2_cabserver/definitions/HAL2_CABSERVER_3.wsdl");
 		$this->buildCommonParams();
 	}
-	
+
 	function buildCustomerData($user = "", $passwd = "") {
 		$this->customerData["Phone"] = $user;
 		$this->customerData["Password"] = $passwd;
-		
+
 		return $this->customerData;
 	}
-	
+
 	function buildGeoPos($lat = 0.0, $lng = 0.0) {
 		if($lat != 0.0) {
-			$this->geoPos["Latitude"]	= $lat;
+			$this->geoPos["Latitude"] = $lat;
 		}
-		
+
 		if($lng != 0.0) {
-			$this->geoPos["Longitude"]	= $lng;
+			$this->geoPos["Longitude"] = $lng;
 		}
-		
+
 		if($this->geoPos["Latitude"] != 0.0 && $this->geoPos["Longitude"] != 0.0) {
 			return $this->geoPos;
 		}
 	}
-	
+
 	function buildCommonParams() {
 		$this->commonParams["UserData"] = array (
 			"User" => "t_cab_android",
@@ -39,63 +39,75 @@ class phpABike {
 		$this->commonParams["languageUID"] = 1;
 		$this->commonParams["RequestTime"] = date("Y-d-m\TG:i:s", time());
 		$this->commonParams["Version"] = 1;
-		
+
 		return $this->commonParams;
 	}
-	
+
 	function buildNewCustomerData() {}
-	
+
 	function buildPaymentByWire() {}
-	
+
 	function buildPaymentByCreditCard() {}
-	
+
 	function buildBounusCard() {}
-	
+
 	function buildTripLimits() {}
-	
+
 	function buildDamageData() {}
-	
+
 	function listFreeBikes($maxRes = 100, $radius = 5000, $lat = 0.0, $lng = 0.0) {
 		$this->result = $this->client->__soapCall("CABSERVER.listFreeBikes",
-						array("CommonParams" => $this->buildCommonParams(),
-							  "SearchPosition" => $this->buildGeoPos($lat, $lng),
-							  "maxResults" => $maxRes,
-							  "searchRadius" => $radius));
-		
+			array("CommonParams" => $this->buildCommonParams(),
+				"SearchPosition" => $this->buildGeoPos($lat, $lng),
+				"maxResults" => $maxRes,
+				"searchRadius" => $radius));
+
 		return $this->result;
 	}
-	
+
 	function getCustomerInfo() {}
-	
+
 	function listReturnLocations () {}
-	
+
 	function rentBike($bike, $user = "", $passwd = "") {
 		$this->result = $this->client->__soapCall("CABSERVER.rentBike",
-						array("Bike" => $bike,
-							  "CustomerData" => $this->buildCustomerData($user, $passwd),
-							  "CommonParams" => $this->buildCommonParams()));
-		
+			array("Bike" => $bike,
+				"CustomerData" => $this->buildCustomerData($user, $passwd),
+				"CommonParams" => $this->buildCommonParams()));
+
 		return $this->result;
 	}
-	
+
 	function returnBike() {}
-	
-	function requestNewPassword() {}
-	
+
+	function requestNewPassword($phone) {
+		$this->result = $this->client->__soapCall("CABSERVER.listFreeBikes",
+			array("CommonParams" => $this->buildCommonParams(),
+				"Phone" => $phone));
+
+		return $this->result;
+	}
+
 	function listProductInfo() {}
-	
+
 	function checkTripStart() {}
-	
+
 	function changePersCode() {}
-	
-	function getBikeInfo() {}
-	
+
+	function getBikeInfo($bike) {
+		$this->result = $this->client->__soapCall("CABSERVER.listFreeBikes",
+			array("CommonParams" => $this->buildCommonParams(),
+				"Bike" => $bike));
+
+		return $this->result;
+	}
+
 	function redeemBonusCode() {}
-	
+
 	function addCustomer() {}
-	
+
 	function reportDamage() {}
-	
+
 	function listCompletedTrips() {}
 }
 ?>
